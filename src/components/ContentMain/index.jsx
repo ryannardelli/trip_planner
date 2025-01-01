43c543a20c0a6f81b331c2f036c8f2ce65d56ac6
Ryan <ryannarelli12@gmail.com>
2025-01-01 00:13:37 -0300

    enviando alterações

--- src/components/ContentMain/index.jsx
+++ src/components/ContentMain/index.jsx
@@ -6,7 +6,7 @@ export const ContentMain = () => {
   return (
     <div className="container" id="container-main">
       <div className="col-12" id="container-header">
-        <h1>Informe o seu CEP</h1>
+        <h1>Informe o CEP</h1>
       </div>
 
       <div className="col-12">
